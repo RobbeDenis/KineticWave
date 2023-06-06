2868bfbac1d600950dcf9b1130fceb90dc9750d7
RobbeDenis <robbe.denis@gmail.com>
2023-06-07 00:44:56 +0200

    Can now load sample and play it

--- Source/MainComponent.cpp
+++ Source/MainComponent.cpp
@@ -12,11 +12,44 @@ MainComponent::MainComponent()
     , m_SquarePos{ 180, 20, 0, 0 }
     , m_CircleRadius{ 30 }
     , m_FontSize{ 24 }
+    , m_SmoothingFactor{ 0.2f }
 {
     setSize (800, 600);
 
     initKinectInfoUI();
+    initLoadUI();
+    initKinectTrackingUI();
 
+    initInputChannels();
+    initKinect();
+
+    enableUpdate(true);
+
+    m_FormatManager.registerBasicFormats();
+    m_TransportSource.addChangeListener(this);
+}
+
+void MainComponent::initLoadUI()
+{
+    addAndMakeVisible(&m_OpenButton);
+    m_OpenButton.setButtonText("Open...");
+    m_OpenButton.onClick = [this] { loadFile(); };
+
+    addAndMakeVisible(&m_PlayButton);
+    m_PlayButton.setButtonText("Play");
+    m_PlayButton.onClick = [this] { changeState(Starting); };
+    m_PlayButton.setColour(juce::TextButton::buttonColourId, juce::Colours::green);
+    m_PlayButton.setEnabled(false);
+
+    addAndMakeVisible(&m_StopButton);
+    m_StopButton.setButtonText("Stop");
+    m_StopButton.onClick = [this] { changeState(Stopping); };
+    m_StopButton.setColour(juce::TextButton::buttonColourId, juce::Colours::red);
+    m_StopButton.setEnabled(false);
+}
+
+void MainComponent::initKinectTrackingUI()
+{
     addAndMakeVisible(m_LeftHand);
     m_LeftHand.setFont(juce::Font(m_FontSize, juce::Font::bold));
     m_LeftHand.setText("L", juce::dontSendNotification);
@@ -26,11 +59,6 @@ MainComponent::MainComponent()
     m_RightHand.setFont(juce::Font(m_FontSize, juce::Font::bold));
     m_RightHand.setText("R", juce::dontSendNotification);
     m_RightHand.setColour(juce::Label::textColourId, juce::Colours::yellow);
-
-    initInputChannels();
-    initKinect();
-
-    enableUpdate(true);
 }
 
 void MainComponent::initKinectInfoUI()
@@ -92,7 +120,6 @@ void MainComponent::initInputChannels()
 
 MainComponent::~MainComponent()
 {
-    // This shuts down the audio device and clears the audio source.
     shutdownAudio();
 }
 
@@ -105,6 +132,7 @@ void MainComponent::prepareToPlay (int samplesPerBlockExpected, double sampleRat
     // but be careful - it will be called on the audio thread, not the GUI thread.
 
     // For more details, see the help for AudioProcessor::prepareToPlay()
+    m_TransportSource.prepareToPlay(samplesPerBlockExpected, sampleRate);
 }
 
 void MainComponent::getNextAudioBlock (const juce::AudioSourceChannelInfo& bufferToFill)
@@ -115,7 +143,13 @@ void MainComponent::getNextAudioBlock (const juce::AudioSourceChannelInfo& buffe
 
     // Right now we are not producing any data, in which case we need to clear the buffer
     // (to prevent the output of random noise)
-    bufferToFill.clearActiveBufferRegion();
+    if (m_pReaderSource.get() == nullptr)
+    {
+        bufferToFill.clearActiveBufferRegion();
+        return;
+    }
+
+    m_TransportSource.getNextAudioBlock(bufferToFill);
 }
 
 void MainComponent::releaseResources()
@@ -124,6 +158,7 @@ void MainComponent::releaseResources()
     // restarted due to a setting change.
 
     // For more details, see the help for AudioProcessor::releaseResources()
+    m_TransportSource.releaseResources();
 }
 
 void MainComponent::paint (juce::Graphics& g)
@@ -148,22 +183,58 @@ void MainComponent::resized()
     m_InfoRX.setBounds(m_InfoPos.x + offsetX, m_InfoPos.y + 88 + offset, 200, 50);
     m_InfoRY.setBounds(m_InfoPos.x + offsetX, m_InfoPos.y + 110 + offset, 200, 50);
 
+    m_OpenButton.setBounds(200, 0, 50, 30);
+    m_PlayButton.setBounds(200, 40, 50, 30);
+    m_StopButton.setBounds(200, 80, 50, 30);
+
     renderKinectTracking();
+}
+
+void MainComponent::loadFile()
+{
+    m_Chooser = std::make_unique<juce::FileChooser>("Select a Wave file",
+        juce::File{},
+        "*.wav");
+
+    auto chooserFlags = juce::FileBrowserComponent::openMode | juce::FileBrowserComponent::canSelectFiles;
+
+    m_Chooser->launchAsync(chooserFlags, [this](const juce::FileChooser& fc)
+        {
+            auto file = fc.getResult();
+
+            if (file != juce::File{})
+            {
+                auto* reader = m_FormatManager.createReaderFor(file);
+
+                if (reader != nullptr)
+                {
+                    auto newSource = std::make_unique<juce::AudioFormatReaderSource>(reader, true);
+                    m_TransportSource.setSource(newSource.get(), 0, nullptr, reader->sampleRate);
+                    m_PlayButton.setEnabled(true);
+                    m_pReaderSource.reset(newSource.release());
+
+                    m_pReaderSource->setLooping(true);
+                }
+            }
+        });
 }
 
 void MainComponent::timerCallback()
 {
     m_pTracker->Update();
-    m_LeftWrist = m_pTracker->GetTrackingJointPosition(NUI_SKELETON_POSITION_WRIST_LEFT);
-    m_RightWrist = m_pTracker->GetTrackingJointPosition(NUI_SKELETON_POSITION_WRIST_RIGHT);
+    Vector4 left = m_pTracker->GetTrackingJointPosition(NUI_SKELETON_POSITION_WRIST_LEFT);
+    Vector4 right = m_pTracker->GetTrackingJointPosition(NUI_SKELETON_POSITION_WRIST_RIGHT);
 
-    m_LeftWrist.x = map(m_LeftWrist.x, -1, 0, 0, 1);
-    //m_LeftWrist.y = map(m_LeftWrist.y, -1, 0, 0, 1);
-    m_LeftWrist.y = std::clamp(1 - map(m_LeftWrist.y, -0.5, 0.5, 0, 1), 0.f, 1.f);
+    left.x = map(left.x, -1, 0, 0, 1);
+    left.y = std::clamp(1 - map(left.y, -0.5, 0.5, 0, 1), 0.f, 1.f);
 
-    m_RightWrist.x = map(m_RightWrist.x, -0.5, 0.5, 0, 1);
-    //m_RightWrist.y = map(m_RightWrist.y, -0.5, 0.5, 0, 1);
-    m_RightWrist.y = std::clamp(1 - map(m_RightWrist.y, -0.5, 0.5, 0, 1), 0.f, 1.f);
+    right.x = map(right.x, -0.5, 0.5, 0, 1);
+    right.y = std::clamp(1 - map(right.y, -0.5, 0.5, 0, 1), 0.f, 1.f);
+
+    smooth(m_LeftWrist.x, left.x);
+    smooth(m_LeftWrist.y, left.y);
+    smooth(m_RightWrist.x, right.x);
+    smooth(m_RightWrist.y, right.y);
 
     renderKinectTracking();
 
@@ -172,6 +243,17 @@ void MainComponent::timerCallback()
     m_InfoLY.setText("LY: " + juce::String::formatted("%f", m_LeftWrist.y), juce::dontSendNotification);
     m_InfoRX.setText("RX: " + juce::String::formatted("%f", m_RightWrist.x), juce::dontSendNotification);
     m_InfoRY.setText("RY: " + juce::String::formatted("%f", m_RightWrist.y), juce::dontSendNotification);
+}
+
+void MainComponent::changeListenerCallback(juce::ChangeBroadcaster* source)
+{
+    if (source == &m_TransportSource)
+    {
+        if (m_TransportSource.isPlaying())
+            changeState(Playing);
+        else
+            changeState(Stopped);
+    }
 }
 
 void MainComponent::renderKinectTracking()
@@ -213,4 +295,39 @@ float MainComponent::map(float value, float min, float max, float toMin, float t
 {
     value = std::clamp(value, min, max);
     return toMin + (value - min) * (toMax - toMin) / (max - min);
+}
+
+void MainComponent::smooth(float& input, float target)
+{
+    input = input + ((target - input) * m_SmoothingFactor);
+}
+
+void MainComponent::changeState(TransportState newState)
+{
+    if (m_State != newState)
+    {
+        m_State = newState;
+
+        switch (m_State)
+        {
+        case Stopped:
+            m_StopButton.setEnabled(false);
+            m_PlayButton.setEnabled(true);
+            m_TransportSource.setPosition(0.0);
+            break;
+
+        case Starting:
+            m_PlayButton.setEnabled(false);
+            m_TransportSource.start();
+            break;
+
+        case Playing:
+            m_StopButton.setEnabled(true);
+            break;
+
+        case Stopping:
+            m_TransportSource.stop();
+            break;
+        }
+    }
 }
--- Source/MainComponent.h
+++ Source/MainComponent.h
@@ -3,8 +3,16 @@
 #include <JuceHeader.h>
 #include "KSkeletonTracker.h"
 
-class MainComponent final : public juce::AudioAppComponent, private juce::Timer
+class MainComponent final : public juce::AudioAppComponent, private juce::Timer, private juce::ChangeListener
 {
+    enum TransportState
+    {
+        Stopped,
+        Starting,
+        Playing,
+        Stopping
+    };
+
 public:
     MainComponent();
     ~MainComponent();
@@ -16,16 +24,37 @@ public:
     void paint (juce::Graphics& g) override;
     void resized() override;
 
+    void changeListenerCallback(juce::ChangeBroadcaster* source) override;
+
 private:
     void initInputChannels();
     void initKinectInfoUI();
+    void initLoadUI();
+    void initKinectTrackingUI();
     void initKinect();
+
     void renderKinectTracking();
     void enableUpdate(bool enable);
     void setUpdateInterval(int intervalMs);
     void timerCallback() override;
 
+    void loadFile();
+    void changeState(TransportState newState);
+
     float map(float value, float min, float max, float toMin, float toMax);
+    void smooth(float &input, float target);
+
+    // Sample
+    juce::AudioFormatManager m_FormatManager;
+    juce::AudioTransportSource m_TransportSource;
+    std::unique_ptr<juce::AudioFormatReaderSource> m_pReaderSource;
+
+    // Loading sample
+    std::unique_ptr<juce::FileChooser> m_Chooser;
+    juce::TextButton m_OpenButton;
+    juce::TextButton m_PlayButton;
+    juce::TextButton m_StopButton;
+    TransportState m_State;
 
     // Kinect
     std::unique_ptr<KSkeletonTracker> m_pTracker;
@@ -50,6 +79,9 @@ private:
     int m_SquareWidth;
     int m_CircleRadius;
     int m_FontSize;
+
+    // Other
+    float m_SmoothingFactor;
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (MainComponent)
 };
